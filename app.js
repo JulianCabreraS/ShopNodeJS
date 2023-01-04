7f78e00c7fa3f9d1d498b33d97f702e06685d22d
jcabrera <julian.cabreras@gmail.com>
2023-01-04 16:32:55 -0600

    Handlebars implementations
    Signed-off-by: jcabrera <julian.cabreras@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require("path");
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars')
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,16 @@ const adminData = require('./admin')
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'})
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 module.exports = router;
