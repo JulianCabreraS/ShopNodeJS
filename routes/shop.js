6667e1bd061f1f4d1c2abf1692b935ee30e0489c
jcabrera <julian.cabreras@gmail.com>
2023-01-06 15:26:50 -0600

    Adding back HTML
    Signed-off-by: jcabrera <julian.cabreras@gmail.com>

--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,6 @@ const rootDir = require('../util/path');
 const adminData = require('./admin')
 const router = express.Router();
 
-
 router.get('/', (req, res, next) => {
     const products = adminData.products;
     res.render('shop', {
